28c56eb073e45a6a04b0da0aef1cccbf820a093c
Jatin <jatin333x@gmail.com>
2025-03-16 06:45:48 +0000

    closure

---
+++ Demo1.java
@@ -0,0 +1,10 @@
+// package .git;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int a=  10;
+        int b=  10;
+        System.out.println(a+b);
+        System.out.println(a*b);
+    }
+}
